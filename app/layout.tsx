0c1c7d752a7fc807807df45e455b8fe27c84d3eb
purboindra <purboindra@gmail.com>
2023-07-24 06:01:02 +0700

    change font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import Sidebar from "./components/Sidebar";
 import ModalProvider from "./components/modals/ModalProvider";
 
-const inter = Inter({ subsets: ["latin"] });
+const monserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "My Kostan",
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={monserrat.className}>
         <ModalProvider />
         <Sidebar>{children}</Sidebar>
       </body>
