416adf7b4d72020d4d5edb8bd576ea3d36269e58
purboindra <purboindra@gmail.com>
2023-07-29 11:46:31 +0700

    fix api post route

--- app/playground/api/signup/route.ts
+++ app/playground/api/signup/route.ts
@@ -3,7 +3,11 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   try {
     const body = await req.json();
+    return NextResponse.json(body, {
+      status: 200,
+    });
   } catch (error: any) {
+    console.log("ERROR SIGN UP", error);
     return new NextResponse("Internal Server Error", {
       status: 500,
     });
