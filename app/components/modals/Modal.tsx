2b27f261183574a74b5d3a3e5ca54d99ce69be80
purboindra <purboindra@gmail.com>
2023-07-30 07:21:44 +0700

    fix height modal

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -21,7 +21,7 @@ const Modal: React.FC<IModalProps> = ({
     <Dialog.Root open={isOpen} defaultOpen={isOpen} onOpenChange={onChange}>
       <Dialog.Portal>
         <Dialog.Overlay className="bg-blackColor bg-opacity-30 backdrop-blur-sm inset-0 fixed">
-          <Dialog.Content className="bg-white py-[25px] px-12 focus:outline-none fixed drop-shadow-md border-[1px] border-[#C4C4C4] rounded-xl top-[50%] left-[50%] max-h-full h-full md:h-auto md:max-h-[85vh] w-full md:w-[90vw] md:max-w-[450px] translate-x-[-50%] translate-y-[-50%]">
+          <Dialog.Content className="bg-white py-[25px] px-12 focus:outline-none fixed drop-shadow-md border-[1px] border-[#C4C4C4] rounded-xl top-[50%] left-[50%] h-full md:h-auto md:max-h-auto w-full md:w-[90vw] md:max-w-[450px] translate-x-[-50%] translate-y-[-50%]">
             {title && <Dialog.Title>{title}</Dialog.Title>}
             {description && (
               <Dialog.Description>{description}</Dialog.Description>
